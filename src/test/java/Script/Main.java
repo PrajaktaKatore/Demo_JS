9da8ca16b9edafb6d9eae2903c763317cba8ba5e
prajaktaka <studymailid12@gmail.com>
2025-01-07 14:13:04 +0530

    commit

--- src/test/java/Script/Main.java
+++ src/test/java/Script/Main.java
@@ -1,58 +1,39 @@
-package Script;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-import Pages.Register;
-import Pages.LogIn;
-import Pages.Search;
-
-
-public class Main
-{
+public class Main {
+	
 	WebDriver driver;
-	Register register;
-	LogIn login;
-	Search add;
+	@BeforeTest
+	public void TestSetUp() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		
-		@BeforeTest
-		public void TestSetUp() throws InterruptedException
-		{
-			//System.setProperty("webdriver.chrome.driver","E:/Selenium/Chrome/chromedriver.exe");
-			//WebDriverManager.chromedriver().setup();
-			WebDriverManager.edgedriver().setup();
-			
-			driver = new EdgeDriver();
-			
-			driver.get("https://demo.nopcommerce.com/");	
-			driver.manage().window().maximize();
-		
-		}
-		
-		@Test
-	    public void Testng() throws InterruptedException 
-	    {	
-			register=new Register(driver);
-			register.Click_On_Register();
-		}
-		@Test
-		public void Testng1() throws InterruptedException 
-		{
-			login= new LogIn(driver);
-			login.Click_On_Log_In();
-		}
-		@Test
-		public void Testng2() throws InterruptedException 
-		{
-			add= new Search(driver);
-			add.Click_On_Add();
-		}
+		driver.get("https://demo.nopcommerce.com/");	
+		driver.manage().window().maximize();
+	
+	}
+	
+	@Test
+    public void Testng() throws InterruptedException 
+    {	
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.findElement(By.partialLinkText("Log In")).click();
 		
 		
-}
+		
+		driver.findElement(By.name("Email")).sendKeys("studymailid12@gmail.com");
+		driver.findElement(By.name("Password")).sendKeys("abc@123");
+		driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+	}
+	
 
+}
